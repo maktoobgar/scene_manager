1b782c1278702c371a16128809e4e41d56c56561
Mahmood Heidari <mahmoodh1378@gmail.com>
2022-03-12 09:53:39 +0330

    🔧 refactor: validate_key returns nothing anymore

--- addons/scene_manager/scene_manager.gd
+++ addons/scene_manager/scene_manager.gd
@@ -2,13 +2,11 @@ extends MarginContainer
 
 onready var Menu: Dictionary = get_node("VBoxContainer/ScrollContainer/container/list").menu_data
 
-func validate_key(key: String) -> bool:
-	var res: bool = Menu.has(key)
+func validate_key(key: String) -> void:
 	assert(
-		res == true,
+		Menu.has(key) == true,
 		"ERROR: `%s` key is not recognized, please double check."% key
 	)
-	return res
 
 func change_scene(key: String) -> void:
 	if Menu.has(key):
